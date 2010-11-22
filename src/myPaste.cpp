411b5656f74298ba5ef694f911c4b4afb05f169f
Bernhard Meindl <bernhard.meindl@statistik.gv.at>
2010-11-22 07:51:18 +0000

    version 0.0.14

--- src/myPaste.cpp
+++ src/myPaste.cpp
@@ -1,27 +1,22 @@
 #include <Rcpp.h>
-#include <string>
-using namespace std;
+using namespace Rcpp ;
 
-RcppExport SEXP myPaste(SEXP stringvec, SEXP outvec, SEXP param) {
-	SEXP rl = R_NilValue; // return list
-
-	RcppStringVector stringVec(stringvec);
-	RcppStringVector outVec(outvec);
-	
-	RcppParams rp(param);
-	int keyVars = rp.getIntValue("nrKeyVars");
+RcppExport SEXP myPaste(SEXP stringvec, SEXP nrKeyVars) {
+	BEGIN_RCPP
+    CharacterVector stringVec(stringvec);
+	int keyVars = as<int>( nrKeyVars ) ;
 	int nrRows = stringVec.size();
-
 	int by = nrRows / keyVars;
+	
+	CharacterVector outVec( by ) ;
+	std::string str;
 	for (int i=0; i < by; i++) {
-		std::string str;
+	    str.clear() ;
 		for(int j=0; j < keyVars; j++) {
-			str.append(stringVec(i+by*j));			
+			str.append(stringVec[i+by*j]);			
 		}
-		outVec(i) = str;
-	}	
-	RcppResultSet rs;
-   	rs.add("str", outVec);
-	rl = rs.getReturnList();
-	return rl;
+		outVec[i] = str;
+	}
+    return outVec ;
+	END_RCPP
 }
